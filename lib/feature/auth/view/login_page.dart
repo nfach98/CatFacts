4f506abe8c48f293ade9b6305968b6f928b44890
Nino Fachrurozy <48285366+nfach98@users.noreply.github.com>
2024-08-28 14:23:12 +0700

    feat(home): style login page

--- lib/feature/auth/view/login_page.dart
+++ lib/feature/auth/view/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cat_facts/config/route_config.dart';
 import 'package:cat_facts/feature/auth/controller/auth_controller.dart';
 import 'package:flutter/material.dart';
@@ -36,70 +38,106 @@ class LoginPage extends GetView<AuthController> {
   }
 
   Widget _buildForm({bool isError = false}) {
-    return Padding(
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.black45,
+            Colors.black87,
+          ],
+        ),
+      ),
       padding: EdgeInsets.all(20),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          TextFormField(
-            initialValue: controller.username.value,
-            onChanged: (value) {
-              controller.setUsername(value);
-            },
-            decoration: InputDecoration(
-              hintText: 'Username',
-            ),
-          ),
-          SizedBox(height: 12),
-          TextFormField(
-            initialValue: controller.password.value,
-            obscureText: controller.isHidePassword.value,
-            onChanged: (value) {
-              controller.setPassword(value);
-            },
-            decoration: InputDecoration(
-              hintText: 'Password',
-              suffixIcon: InkWell(
-                onTap: () {
-                  controller.togglePassword();
-                },
-                child: Icon(
-                  controller.isHidePassword.value
-                      ? Icons.visibility
-                      : Icons.visibility_off,
+          ClipRRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+              child: Container(
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.white.withOpacity(.5),
+                      Colors.white.withOpacity(.2),
+                    ],
+                    begin: AlignmentDirectional.topStart,
+                    end: AlignmentDirectional.bottomEnd,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  border: Border.all(
+                    width: 1.5,
+                    color: Colors.white.withOpacity(0.2),
+                  ),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      initialValue: controller.username.value,
+                      onChanged: (value) {
+                        controller.setUsername(value);
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    TextFormField(
+                      initialValue: controller.password.value,
+                      obscureText: controller.isHidePassword.value,
+                      onChanged: (value) {
+                        controller.setPassword(value);
+                      },
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            controller.togglePassword();
+                          },
+                          child: Icon(
+                            controller.isHidePassword.value
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    FractionallySizedBox(
+                      widthFactor: 1,
+                      child: TextButton(
+                        onPressed: () async {
+                          final isSuccess = await controller.login();
+                          if (isSuccess) {
+                            Get.rootDelegate.offNamed(RouteConfig.homeRoute);
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll<Color>(
+                            Colors.black87,
+                          ),
+                          foregroundColor: MaterialStatePropertyAll<Color>(
+                            Colors.white,
+                          ),
+                        ),
+                        child: Text('Login'),
+                      ),
+                    ),
+                    if (isError)
+                      Padding(
+                        padding: EdgeInsets.only(top: 12),
+                        child: Text(
+                          'There is error on login process, try again',
+                          style: TextStyle(color: Colors.red),
+                        ),
+                      ),
+                  ],
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 20),
-          FractionallySizedBox(
-            widthFactor: 1,
-            child: TextButton(
-              onPressed: () async {
-                final isSuccess = await controller.login();
-                if (isSuccess) {
-                  Get.rootDelegate.offNamed(RouteConfig.homeRoute);
-                }
-              },
-              style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll<Color>(
-                  Colors.purple,
-                ),
-                foregroundColor: MaterialStatePropertyAll<Color>(
-                  Colors.white,
-                ),
-              ),
-              child: Text('Login'),
-            ),
-          ),
-          if (isError)
-            Padding(
-              padding: EdgeInsets.only(top: 12),
-              child: Text(
-                'There is error on login process, try again',
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
+          )
         ],
       ),
     );
